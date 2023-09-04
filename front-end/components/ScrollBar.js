14d7100746df9b60abbb321b8925d2d17f33de71
seanadeymccaul <103879392+seanadeymccaul@users.noreply.github.com>
2023-09-04 19:37:21 +1000

    Add Refresh Control

--- front-end/components/ScrollBar.js
+++ front-end/components/ScrollBar.js
@@ -5,13 +5,29 @@ function ScrollBar() {
 
     // State for refresh control
 
+        // Use the state
+        const [refreshing, setRefreshing] = React.useState(false);
+
+        // On refresh
+        const onRefresh = React.useCallback(() => {
+            setRefreshing(true);
+            setTimeout(() => {
+            setRefreshing(false);
+            }, 2000);
+        }, []);
+
     return(
 
         <SafeAreaView>
 
             <ScrollView 
                 contentContainerStyle={{paddingBottom:"56%"}}
-            >
+                refreshControl={
+                    <RefreshControl
+                        refreshing={refreshing}
+                        onRefresh={onRefresh}
+                    />
+            }>
 
             <Text style={{textAlign: 'center'}}>Test Scroll Bar</Text>
         
